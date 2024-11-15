adaa8e08c11afd7b78308cc233138695d4db94bc
gilmaramg66 <gialmuno@espol.edu.ec>
2024-11-14 20:02:52 -0500

    Update script.sh
    Terminado, hace falta probarlo

--- script.sh
+++ script.sh
@@ -5,8 +5,10 @@ if[-z "$1"]; then
   exit 1
 fi
 
+#se designa el parametro como la variable ubicación
 location="$1"
 
+#pide una ubicación hasta que esta exista
 until [-f "$location"]; do
   echo "El archivo no existe en la ubicación especificada. Ingrese una nueva ubicación: "
   read location
@@ -14,6 +16,25 @@ done
 
 echo "Archivo encontrado en $location"
 
+#asigna el directorio de la ubicación proporcionada
 dir=$(dirname "$location")
 
-ls -l --inode "$directory" > "$directory" > "$directory/original.txt"
+#guarda el primer archivo con los detalles del directorio
+ls -l --inode "$dir" > "$dir/original.txt"
+
+echo "Archivo original.txt ha sido creado con el listado de archivos y carpetas"
+
+#Se ejecutan los cambios en los permisos
+
+echo "Cambiando permisos de archivos en el directorio principal"
+find "$dir" -maxdepth 1 -type f -exec chmod 743 {} \;
+
+echo "Cambiando permisos de archivos en los subidrectorios"
+find "$dir" -mindepth 2 -type f -exec chmod 542 {} \;
+
+#se guarda el nuevo archivo con los cambios y los detalles de los subdirectorios
+
+ls -lR --inode "$dir" > "$dir/final.txt"
+
+echo "Archivo final.txt ha sido creado con el listado de archivos y carpetas en el directorio y subdirectorios"
+
